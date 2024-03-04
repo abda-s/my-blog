dcf41d657fe1ae8c61c463c28f4ea72023991bec
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-04 08:06:14 -0800

    making the links in the nav bar in an object

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styls/Navbar.css"
+
+const Navlinks = [
+    {
+        title: "Home.",
+        link: "/"
+    },
+    {
+        title: "About Me.",
+        link: "/AboutMe"
+    },
+    {
+        title: "Coding.",
+        link: "/Coding"
+    },
+    {
+        title: "Youtube.",
+        link: "/Youtube"
+    },
+    {
+        title: "Art.",
+        link: "/Art"
+    }
+]
+
 const NavigationBar = () => {
     return (
         <div className="navbar"> {/* Use className instead of class */}
             <h1 className="logo">Abdullah Salameh.</h1>
             <ul>
-                <li><Link to="/" className="coloer-blue">Home.</Link></li>
-                <li><Link to="/AboutMe" className="coloer-orange">About Me.</Link></li>
-                <li><Link to="/Coding" className="coloer-green">Coding.</Link></li>
-                <li><Link to="/Youtube" className="coloer-red">Youtube.</Link></li>
-                <li><Link to="/Art" className="coloer-blue">Art.</Link></li>
+                {Navlinks.map((item, index) => (
+                    <li key={index}>
+                        <Link to={item.link} >{item.title}</Link>
+                    </li>))}
             </ul>
         </div>
     )

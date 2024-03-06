765a2a70e43c210936da59297ab05a572c8eefdb
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-06 10:25:15 -0800

    adding an avatar and making the nav bar responsive

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styls/Navbar.css"
-
+import profile from "../imgs/prfile.jpg"
 const Navlinks = [
     {
         title: "Home.",
@@ -24,17 +24,24 @@ const Navlinks = [
         link: "/Art"
     }
 ]
-
 const NavigationBar = () => {
+    const [menuActive, setMenueActive] = useState(false);
+
     return (
         <div className="navbar"> {/* Use className instead of class */}
-            <h1 className="logo">Abdullah Salameh.</h1>
-            <ul  className="active">
-                {Navlinks.map((item, index) => (
-                    <li key={index}>
-                        <Link  to={item.link} >{item.title}</Link>
-                    </li>))}
-            </ul>
+            <div className="menu-and-logo">
+                <h1 className="logo">Abdullah Salameh.</h1>
+                <i className="fa-solid fa-bars menu-icon" onClick={() => setMenueActive(!menuActive)}></i>
+            </div>
+            <div className={`menu-content-container ${menuActive && "active"}`}>
+                <ul>
+                    {Navlinks.map((item, index) => (
+                        <li key={index}>
+                            <Link to={item.link} >{item.title}</Link>
+                        </li>))}
+                </ul>
+                <img className="profile-pic" src={profile} />
+            </div>
         </div>
     )
 }

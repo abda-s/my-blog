5bd2b095c46d3c89770404beb7ca4ea4cfca4281
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-14 21:18:09 +0300

    making the header in the posts page in the center

--- src/components/PostsView.jsx
+++ src/components/PostsView.jsx
@@ -2,22 +2,14 @@ import React from "react";
 import OnePost from "./OnePost";
 import "../styls/posts-view.css"
 import posts from "../moks/posts"
-// const posts = [
-//     {
-//         title: "React Blog",
-//         date: "13/3/24",
-//         description: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-//         tags: ["react", "arduino"],
-//         link: "/"
-//     },
-// ] 
+
 export default function PostsView() {
     return (
         <div className="all-posots-container">
-        <div className="header">
-        PostsView 
-        </div>
-            
+            <div className="header">
+                Recent Posts
+            </div>
+
             <div className="posots-container">
                 {posts.map((post, index) => {
                     return <OnePost

16e209c51c35dce5627582b59244b4233928f7a0
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-15 22:52:46 +0300

    creating signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import AboutMe from "./components/AboutMe";
 import Posts from "./components/Posts";
 import Login from "./components/Login"
+import Signup from "./components/Signup"
 
 import "./styls/main.css"
 
@@ -20,6 +20,7 @@ const App = () => {
           <Route path="/AboutMe" element={<AboutMe />} />
           <Route path="/Posts" element={<Posts />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>

e018fc7b051fc7137b05e6184ac22332a968853c
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-25 19:45:44 +0300

    fixing the date and makeing the edit button disapear when you are not logged in

--- src/components/OnePost.jsx
+++ src/components/OnePost.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
-import "../styls/one-post.css";
+import { AuthContext } from "../helpers/AuthContext";
 import TruncatedText from "./TruncatedText";
+import "../styls/one-post.css";
 
 //"https://wallpaperbat.com/img/641219-react-js-wallpaper-top-free-react-js-background.jpg"
 export default function OnePost(props) {
+  const [convertedDate, setConvertedDate] = useState("");
+  useEffect(() => {
+    const inputDateString = props.date;
+    const inputDate = new Date(inputDateString);
+
+    // Extract year, month, and day components
+    const year = inputDate.getFullYear();
+    const month = String(inputDate.getMonth() + 1).padStart(2, "0"); // Adding 1 because getMonth() returns zero-based index
+    const day = String(inputDate.getDate()).padStart(2, "0");
+
+    // Format into desired format
+    const outputDateString = `${year}/${month}/${day}`;
+
+    setConvertedDate(outputDateString);
+  }, []);
+
+  const { authState, setAuthState } = useContext(AuthContext);
+
   return (
     <div className="post-container">
       <img src={props.image} />
@@ -14,13 +32,19 @@ export default function OnePost(props) {
           <h1>{props.title}</h1>
           <TruncatedText text={props.description} maxLength={50} />
         </Link>
-        <Link to={`/EditPost/${props.link}`}>Edit</Link>
         {/* <div className="tags">
           {props.tags.map((item, index) => {
             return <span key={index}>{item}</span>;
           })}
         </div> */}
-        <span className="date">{props.date}</span>
+        <span className="date">{convertedDate}</span>
+        {!authState.state ? (
+          <></>
+        ) : (
+          <Link className="edit-link" to={`/EditPost/${props.link}`}>
+            Edit
+          </Link>
+        )}
       </div>
     </div>
   );

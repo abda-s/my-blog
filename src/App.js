d7cf5e74e7caad6658f6e90e35838bdb99b16205
abdullah salameh <abdullahsalameh66@gmail.com>
2024-03-25 09:25:01 +0300

    connecting to the server and adding auth

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./components/Home";
 import AboutMe from "./components/AboutMe";
 import Posts from "./components/Posts";
-import Login from "./components/Login"
-import Signup from "./components/Signup"
+import Login from "./components/Login";
+// import Signup from "./components/Signup";
 
-import "./styls/main.css"
+import { AuthContext } from "./helpers/AuthContext";
+import axios from "axios";
+
+import "./styls/main.css";
 
 const App = () => {
+  const [authState, setAuthState] = useState({
+    username: "",
+    id: 0,
+    state: false,
+  });
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/auth/auth", {
+        headers: {
+          accessToken: localStorage.getItem("accessToken"),
+        },
+      })
+      .then((response) => {
+        if (response.data.error) {
+          setAuthState({ username: "", id: 0, state: false });
+        } else {
+          setAuthState({
+            username: response.data.username,
+            id: response.data.id,
+            state: true,
+          });
+        }
+      });
+  }, []);
+
   return (
     <div className="all">
-      <BrowserRouter>
-      <NavigationBar />
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <BrowserRouter>
+          <NavigationBar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/AboutMe" element={<AboutMe />} />
-          <Route path="/Posts" element={<Posts />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/AboutMe" element={<AboutMe />} />
+            <Route path="/Posts" element={<Posts />} />
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="/signup" element={<Signup />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </AuthContext.Provider>
     </div>
   );
 };
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,45 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import "../styls/Navbar.css"
-import profile from "../imgs/prfile.jpg"
-const Navlinks = [
-    {
-        title: "Home.",
-        link: "/"
-    },
-    {
-        title: "About Me.",
-        link: "/AboutMe"
-    },
-    {
-        title: "Posts.",
-        link: "/Posts"
-    },
-    {
-        title: "login.",
-        link: "/login"
-    },
-]
-const NavigationBar = () => {
-    const [menuActive, setMenueActive] = useState(false);
+import "../styls/Navbar.css";
+import profile from "../imgs/prfile.jpg";
 
-    return (
-        <div className="navbar"> {/* Use className instead of class */}
-            <div className="menu-and-logo">
-                <h1 className="logo">Abdullah Salameh.</h1>
-                <i className="fa-solid fa-bars menu-icon" onClick={() => setMenueActive(!menuActive)}></i>
-            </div>
-            <div className={`menu-content-container ${menuActive && "active"}`}>
-                <ul>
-                    {Navlinks.map((item, index) => (
-                        <li key={index}>
-                            <Link to={item.link} >{item.title}</Link>
-                        </li>))}
-                </ul>
-                <img className="profile-pic" src={profile} />
-            </div>
-        </div>
-    )
-}
+import { AuthContext } from "../helpers/AuthContext";
+
+const Navlinks = [
+  {
+    title: "Home.",
+    link: "/",
+  },
+  {
+    title: "About Me.",
+    link: "/AboutMe",
+  },
+  {
+    title: "Posts.",
+    link: "/Posts",
+  },
+];
+const NavigationBar = () => {
+  const { authState, setAuthState } = useContext(AuthContext);
+  const [menuActive, setMenueActive] = useState(false);
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({ username: "", id: 0, state: false });
+  };
+  return (
+    <div className="navbar">
+      {" "}
+      {/* Use className instead of class */}
+      <div className="menu-and-logo">
+        <h1 className="logo">Abdullah Salameh.</h1>
+        <i
+          className="fa-solid fa-bars menu-icon"
+          onClick={() => setMenueActive(!menuActive)}
+        ></i>
+      </div>
+      <div className={`menu-content-container ${menuActive && "active"}`}>
+        <ul>
+          {Navlinks.map((item, index) => (
+            <li key={index}>
+              <Link to={item.link}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+        {!authState.state ? (
+          <></>
+        ) : (
+          <>
+            <img className="profile-pic" src={profile} />
+            <button onClick={logout}>log out</button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default NavigationBar;
